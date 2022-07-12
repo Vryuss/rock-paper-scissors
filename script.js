a6ba72dc5d392972beca6af699a9048d33a847c0
Vryuss <tristan.c.meyers@gmail.com>
2022-07-12 09:42:52 -0400

    added looping game function and score counter upon game end

--- script.js
+++ script.js
@@ -18,21 +18,39 @@ function playRound(playerSelection, computerSelection){
     let player = playerSelection.toLowerCase()
     
     if(player == "scissors" && computerSelection == "paper"){
+        win++
         return "You win! Scissors beats Paper"
+        
     }
     if(player == "paper" && computerSelection == "rock"){
+        win++
         return "You win! Paper beats Rock"
+        
     }
     if(player == "rock" && computerSelection == "scissors"){
+        win++;
         return "You win! Rock beats Scissors"
+        
     }
     else{
+        loss++;
         return "You lost. " + computerSelection + " beats " + playerSelection;
+        
     }
 
 }
 
-const playerSelection = "rock";
-const computerSelection = computerPlay();
-console.log(computerSelection);
-console.log(playRound(playerSelection, computerSelection));
+let win = 0;
+let loss = 0;
+
+function game(){
+    for (let i = 0; i < 5; i++){
+        let computerSelection = computerPlay();
+        let playerSelection = prompt("Rock, Paper, or Scissors?");
+
+        console.log(playRound(playerSelection, computerSelection));
+    }
+    console.log("You won " + win + " times and lost " + loss + " times.");
+}
+
+game();
