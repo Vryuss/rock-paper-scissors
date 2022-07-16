676789ef0171f282b3bf89f16eba66ee4d292d70
Vryuss <tristan.c.meyers@gmail.com>
2022-07-16 16:49:51 -0400

    Player and CPU score is displayed on page

--- script.js
+++ script.js
@@ -14,6 +14,9 @@ function computerPlay(){
     
 }
 
+const pScore = document.querySelector(".p1")
+const cpuScore = document.querySelector(".p2")
+
 function playRound(playerSelection, computerSelection){
     let player = playerSelection.toLowerCase()
     
@@ -23,21 +26,25 @@ function playRound(playerSelection, computerSelection){
     }
     if(player == "scissors" && computerSelection == "paper"){
         win++
+        pScore.innerHTML = "You: " + win
         return "You win! Scissors beats Paper"
         
     }
     if(player == "paper" && computerSelection == "rock"){
         win++
+        pScore.innerHTML = "You: " + win
         return "You win! Paper beats Rock"
         
     }
     if(player == "rock" && computerSelection == "scissors"){
         win++;
+        pScore.innerHTML = "You: " + win
         return "You win! Rock beats Scissors"
         
     }
     else{
         loss++;
+        cpuScore.innerHTML = "CPU: " + loss
         return "You lost. " + computerSelection + " beats " + playerSelection;
         
     }
@@ -55,8 +62,8 @@ btn.forEach((button) => {
     button.addEventListener('click', () => {
         const br = document.createElement("br");
         const text = document.createTextNode(playRound(button.id, computerPlay()))
-        output.appendChild(br);
         output.appendChild(text);
+        output.appendChild(br);
         
 });
 })
